96dc63cf457d12f0873ba3941deb2f1849dee429
pyjaru <pyjaru@gmail.com>
2017-06-12 20:30:41 +0900

    28장까지 완료.

--- app/src/main/java/net/pyjaru/photogallery/PhotoPageFragment.java
+++ app/src/main/java/net/pyjaru/photogallery/PhotoPageFragment.java
@@ -6,9 +6,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * Created by pyjaru on 2017. 6. 9..
@@ -19,6 +21,7 @@ public class PhotoPageFragment extends VisibleFragment {
 
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static PhotoPageFragment newInstance(Uri uri){
         Bundle args = new Bundle();
@@ -39,8 +42,24 @@ public class PhotoPageFragment extends VisibleFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_page, container, false);
+
+        mProgressBar = (ProgressBar)view.findViewById(R.id.fragment_photo_page_progress_bar);
+        mProgressBar.setMax(100); //WebChromeClient의 로딩 범위 0~100
+
         mWebView = (WebView)view.findViewById(R.id.fragment_photo_page_web_view);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if(newProgress == 100){
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+        });
+
         mWebView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
@@ -48,6 +67,7 @@ public class PhotoPageFragment extends VisibleFragment {
                 return false;
             }
         });
+
         mWebView.loadUrl(mUri.toString());
         return view;
     }
